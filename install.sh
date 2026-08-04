3279df49acf9c0a12acd468d3cc06584ee845216
caley <195605706+cqley@users.noreply.github.com>
2026-08-04 19:40:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git clone --depth 1 https://github.com/cqley/wallpapers.git /tmp/w
 mkdir -p "$HOME/pictures/wallpapers"
-find /tmp/w -type f \( -name "*.png" -o -name "*.jpg" \) -exec cp {} "$HOME/pictures/wallpapers/" \;
+find /tmp/w -type f \( -name "*.png" -o -name "*.jpg" \) -exec cp {} "$HOME/pictures/wallpapers" \;
 rm -rf /tmp/w
